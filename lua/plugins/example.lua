9738eb8695bb8bee35c84327cbf4ab42adac4b8d
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-07-27 11:30:33 -0400

    Small lsp additions.

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -89,6 +89,8 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        clangd = {},
+        gopls = {},
       },
     },
   },
